53b867deb44b722acbc7702c7727c54ed3edd9d8
Evan Pringle <epringle365@hotmail.com>
2018-08-02 16:17:21 -0400

    added favicon and descriptions

--- js/script.js
+++ js/script.js
@@ -36,12 +36,14 @@ $('footer a, .site-link a, .hover-off').hover(
 
 function openNav() {
 	$('#sideNav').css('width', '100%');
+	$('.sidenav-links').css('opacity', '1');
 	$('body').css('background-color', 'rgba(0,0,0,0.6)');
 	$('#main-content img').css('opacity', '0.3');
 }
 
 function closeNav() {
 	$('#sideNav').css('width', '0px');
+	$('.sidenav-links').css('opacity', '0');
 	$('body').css('background-color', 'white');
 	$('#main-content img').css('opacity', '1');
 }
