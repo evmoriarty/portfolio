846110a5811bde96ccfe0515fe1810964702e586
Evan Pringle <evan.pringle1@gmail.com>
2019-12-13 10:34:24 -0500

    fixed #22

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-$(document).ready(function() {
-
-	/* Scroll Reveal */
-	window.sr = ScrollReveal();
-
-	let options = {
-		duration: 1500,
-		distance: '100px',
-		scale: 1,
-	}
-	sr.reveal('header .text', { duration: 1500, distance: '100px', scale: 1 });
-	sr.reveal('header .image', { duration: 1500, distance: '100px', scale: 1 });
-	// sr.reveal('#row-50 .landing-image', options);
-	sr.reveal('#row-50', options);
-	sr.reveal('#row-100', options);
-	sr.reveal('.works-image-main', options);
-	sr.reveal('.works-image-sub', options);
-	sr.reveal('.works-image-more img', options);
-	sr.reveal('.works-showcase, #row-50', {
-						duration: 1500,
-						delay: 100,
-						distance: '100px',
-						scale: 1,
-						viewFactor: 0.5				// e.g., 0.5 means 50% of the element has to be shown before it becomes visible
-					});
-});
-
 $('footer a, .site-link a, .hover-off').hover(
 	function() {
 		$(this).removeClass('hover-off');
