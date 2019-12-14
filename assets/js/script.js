34478e87cb82bc164b8b93b9c8802a833c75f1a6
SrgSprinkles <evan.pringle1@gmail.com>
2019-12-13 20:07:12 -0500

    fixed #11

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function () {
+	$('#hamburger-icon').click(function () {
+		$(this).toggleClass('active');
+
+		var hamburgerClass = $(this).attr("class");
+
+		if (hamburgerClass.search("close-menu") == -1) {
+			$('.overlay').toggleClass('open');
+			$('body').css('overflow-y', 'hidden');
+			$(this).toggleClass('close-menu');
+			$(this).toggleClass('open-menu');
+		} else {
+			$('.overlay').toggleClass('open');
+			$('body').css('overflow-y', 'auto');
+			$(this).toggleClass('close-menu');
+			$(this).toggleClass('open-menu');
+		}
+	});
+});
+
 $('footer a, .site-link a, .hover-off').hover(
 	function() {
 		$(this).removeClass('hover-off');
@@ -7,20 +27,6 @@ $('footer a, .site-link a, .hover-off').hover(
 		$(this).addClass('hover-off');
 	}
 );
-
-function openNav() {
-	$('#sideNav').css('width', '100%');
-	$('.sidenav-links').css('opacity', '1');
-	$('body').css('background-color', 'rgba(0,0,0,0.6)');
-	$('#main-content img').css('opacity', '0.3');
-}
-
-function closeNav() {
-	$('#sideNav').css('width', '0px');
-	$('.sidenav-links').css('opacity', '0');
-	$('body').css('background-color', 'white');
-	$('#main-content img').css('opacity', '1');
-}
 
 $('#northlich').click(function() {
 	location.href="./northlich.html";
