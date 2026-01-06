3cd56970c73acf85ed0c0f493c664ba4154b3a21
Evan Moriarty <evan.pringle1@gmail.com>
2026-01-05 22:53:22 -0500

    fixed CRLF endings causing problems, and reworked deploy script to not be so hard-coded

--- bin/deploy-site-to-firebase.sh
+++ bin/deploy-site-to-firebase.sh
@@ -13,13 +13,12 @@ INFO="[${BLUE}*${NC}]"
 SUCCESS="[${GREEN}+${NC}]"
 ############################
 
-PROJECT_PATH=/d/workspace/portfolio/
-FIREBASE_DEPLOY_PARENT_DIR=/d/workspace/firebase
-FIREBASE_DEPLOY_PATH=${FIREBASE_DEPLOY_PARENT_DIR}/public/
+if [[ ! -d .git/ ]]; then
+    echo -e "${ERROR} $(pwd) is not the portfolio root directory"
+    exit 1
+fi
 
-pushd $PROJECT_PATH
-
-echo -e "${INFO} Rebuilding site..."
+echo -e "${INFO} Cleaning and building site..."
 
 bash ./bin/clean-site.sh
 bash ./bin/build-site-prod.sh
@@ -29,23 +28,17 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-if [[ ! -d ${PROJECT_PATH}/_site/ ]]; then
-    echo -e "${ERROR} Something weird happened because '${PROJECT_PATH}/_site/' does not exist"
+if [[ ! -d _site/ ]]; then
+    echo -e "${ERROR} Something went wrong building the site because '/_site/' does not exist"
     exit 1
 fi
-
-# Remove previous artifacts and copy the newly built site
-rm -rf ${FIREBASE_DEPLOY_PATH}/*
-cp -R ${PROJECT_PATH}/_site/* ${FIREBASE_DEPLOY_PATH}
-
-pushd ${FIREBASE_DEPLOY_PARENT_DIR}
 
 echo -e "${INFO} Deploying site to Firebase..."
 
 firebase deploy --only hosting
 
 if [[ $? -ne 0 ]]; then
-    echo -e "${ERROR} Something went wrong when attempting to deploy site to Firebase"
+    echo -e "${ERROR} Something went wrong when deploying site to Firebase"
     exit 1
 fi
 
